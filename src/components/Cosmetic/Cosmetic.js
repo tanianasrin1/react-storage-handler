13cb9d1e2bd913362d26432cf803ee7c6be844cf
tanianasrin1 <tasrineva169@gmail.com>
2022-03-21 20:26:05 +0600

    add to card

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -3,11 +3,17 @@ import './Cosmetic.css'
 
 const Cosmetic = (props) => {
     const {name, price, id} = props.cosmetic
+   const addTOCard = (id) =>{
+       console.log('item', id)
+   }
+
+   const addToCardWithParam = () => addTOCard(id)
     return (
         <div className='product'>
             <h2>This by: {name} </h2>
             <p>Only for: ${price}  </p>
             <p>Has id: {id}  </p>
+            <button onClick={addToCardWithParam}>Add to card</button>
         </div>
     );
 };
